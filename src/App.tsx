70d69f57b4212b961d42a918a6ec28a34d0b467d
Req <snogedirl@gmail.com>
2025-11-14 10:00:57 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+    </div>
+  );
+}
+
+export default App;
